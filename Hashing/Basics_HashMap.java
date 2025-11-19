eb3934b4a51af341d6f8a1e442a949765ac34e78
Vaishnavi Singla <vsingla_be24@thapar.edu>
2025-11-19 13:23:49 +0530

    Refine comments on HashMap operations
    Updated comments to clarify time complexity for HashMap operations.

--- Hashing/Basics_HashMap.java
+++ Hashing/Basics_HashMap.java
@@ -6,7 +6,7 @@ class Basics_HashMap
         //Create
         HashMap<String,Integer> age=new HashMap<>();//String is key -> Integer is its value
 
-        //Adding Elements
+        //Adding Elements->T.C=O(1)
         age.put("Akash",21);
         age.put("Yash",16);
         age.put("Luv",17);
@@ -14,41 +14,41 @@ class Basics_HashMap
         age.put("Harry",18);
 
 
-        //Getting value of a key
+        //Getting value of a key->T.C=O(1)
         System.out.println(age.get("Yash"));//16
         System.out.println(age.get("Rocky"));//null as it not exist
         
 
-        //changing or updating value of key
+        //changing or updating value of key->T.C=O(1)
         age.put("Akash",43);
         System.out.println(age.get("Akash"));//43
 
 
-        //removing a pair
+        //removing a pair->T.C=O(1)
         System.out.println(age.remove("Luv"));//17
         System.out.println(age.remove("Rocky"));//null-dont give error
 
 
-        //check if key exist
+        //check if key exist->T.C=O(1)
         System.out.println(age.containsKey("Harry"));//true
         System.out.println(age.containsKey("Rocky"));//false
 
 
-        //add if not present
+        //add if not present->T.C=O(1)
         if(!age.containsKey("Yahika"))//will enter
         age.put("Yashika",30);
         //Alternative or short form:
         age.putIfAbsent("Gautam",50);///will not enter
         
 
-        //Printing
+        //Printing->T.C=O(n)
         System.out.println(age.keySet());//Get all the keys 
         System.out.println(age.values());//Get all the values
         System.out.println(age.entrySet());//or just "age" in place of "age.entrySet"-to get all the values+key
         System.out.println();
 
 
-        //Traverse HashMap
+        //Traverse HashMap->T.C=O(n)
         for(String key:age.keySet())//1st Method
         System.out.printf("Age of %s is %d\n",key,age.get(key)); 
         System.out.println(); 
